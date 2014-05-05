281c3b1ce2b67da53f4f131cee79ad6e75c12636
ltfschoen <ltfschoen@gmail.com>
2014-05-05 15:35:10 +1000

    Created Custom Class “BlogPost” of type NSObject to store Data Structures instead of using NSDictionary

---
+++ BlogReader/BlogPost.h
@@ -0,0 +1,13 @@
+//
+//  BlogPost.h
+//  BlogReader
+//
+//  Created by Luke on 5/05/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlogPost : NSObject
+
+@end
---
+++ BlogReader/BlogPost.m
@@ -0,0 +1,13 @@
+//
+//  BlogPost.m
+//  BlogReader
+//
+//  Created by Luke on 5/05/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import "BlogPost.h"
+
+@implementation BlogPost
+
+@end
